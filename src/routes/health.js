3412442ccf29cc8d44292f93851f6ca41b3f2670
Keith Craley <Keith.Craley@berkadia.com>
2018-08-30 18:18:23 -0400

    Update health Content-type

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,8 +1,9 @@
 import Router from 'express';
+
 const router = new Router();
 
 router.get('/health', (req, res) => {
-  res.set('Content-type', 'text/plain');
+  res.set('Content-type', 'application/json');
   const status = req.app.get('HEALTH_STATUS');
   return res.status(200).send(status);
 });
